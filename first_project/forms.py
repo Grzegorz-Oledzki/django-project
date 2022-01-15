8c48eaed65361c0232cf932be18d9a91320ea30e
grzegorz-oledzki <you@example.com>
2022-01-15 18:13:27 +0100

    black formatting

--- first_project/forms.py
+++ first_project/forms.py
@@ -1,7 +1,8 @@
 from django.forms import ModelForm
 from .models import Player
 
+
 class PlayerForm(ModelForm):
     class Meta:
         model = Player
-        fields = ['title', 'description', 'demo_link', 'source_link', 'tags']
+        fields = ["title", "description", "demo_link", "source_link", "tags"]
--- first_project/views.py
+++ first_project/views.py
@@ -4,8 +4,6 @@ from .models import Player
 from .forms import PlayerForm
 
 
-
-
 def players(request):
     players = Player.objects.all()
     context = {"players": players}
@@ -16,37 +14,38 @@ def player(request, pk):
     playerObj = Player.objects.get(id=pk)
     return render(request, "first_project/single-project.html", {"player": playerObj})
 
+
 def createPlayer(request):
     form = PlayerForm()
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PlayerForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('players')
+            return redirect("players")
 
-    context = {'form': form}
+    context = {"form": form}
     return render(request, "first_project/project_form.html", context)
+
 
 def updatePlayer(request, pk):
     player = Player.objects.get(id=pk)
     form = PlayerForm(instance=player)
 
-    if request.method == 'POST':
+    if request.method == "POST":
         form = PlayerForm(request.POST, instance=player)
         if form.is_valid():
             form.save()
-            return redirect('players')
+            return redirect("players")
 
-    context = {'form': form}
+    context = {"form": form}
     return render(request, "first_project/project_form.html", context)
+
 
 def deletePlayer(request, pk):
     player = Player.objects.get(id=pk)
-    context = {'object': player}
-    if request.method == 'POST':
+    context = {"object": player}
+    if request.method == "POST":
         player.delete()
-        return redirect('players')
-    return render(request, 'first_project/delete_obj.html', context)
-
-
+        return redirect("players")
+    return render(request, "first_project/delete_obj.html", context)
