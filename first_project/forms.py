a32c2f2dd149b3ec8f84974caaa9defa707f2021
grzegorz-oledzki <you@example.com>
2022-01-18 19:26:20 +0100

    black formatiing

--- first_project/forms.py
+++ first_project/forms.py
@@ -5,4 +5,11 @@ from .models import Player
 class PlayerForm(ModelForm):
     class Meta:
         model = Player
-        fields = ["title", "description", "demo_link", "source_link", "tags", 'featured_image']
+        fields = [
+            "title",
+            "description",
+            "demo_link",
+            "source_link",
+            "tags",
+            "featured_image",
+        ]
