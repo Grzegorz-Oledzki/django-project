7d5d1fcf2ce4effd51c50199e19bd94057ca9ec9
grzegorz-oledzki <you@example.com>
2022-01-23 14:26:01 +0100

    modify add player form

--- first_project/forms.py
+++ first_project/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Player
+from django import forms
 
 
 class PlayerForm(ModelForm):
@@ -13,3 +14,17 @@ class PlayerForm(ModelForm):
             "tags",
             "featured_image",
         ]
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(PlayerForm, self).__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input', 'placeholder': 'Add ' + name})
+        # self.fields['title'].widget.attrs.update({'class': 'input', 'placeholder': 'Add name'})
+        # self.fields['description'].widget.attrs.update({'class': 'input', 'placeholder': 'Add description'})
+        # self.fields['demo_link'].widget.attrs.update({'class': 'input', 'placeholder': 'Add demo link'})
+        # self.fields['source_link'].widget.attrs.update({'class': 'input', 'placeholder': 'Add source link'})
+
