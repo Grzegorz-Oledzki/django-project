20c44b4f37ffee3863bdc997ce59217d9a6ed4e3
grzegorz-oledzki <you@example.com>
2022-02-04 19:26:50 +0100

    add registration function to user app

--- users/views.py
+++ users/views.py
@@ -3,37 +3,54 @@ from django.contrib.auth import login, authenticate, logout
 from .models import Profile, Skill
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 
 def loginUser(request):
+    page = "login"
 
     if request.user.is_authenticated:
-        return redirect('profiles')
+        return redirect("profiles")
 
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
+        username = request.POST["username"]
+        password = request.POST["password"]
 
         try:
-            user = User.objects.get(username=username),
+            user = (User.objects.get(username=username),)
         except:
-            messages.error(request, 'Username does not exist')
+            messages.error(request, "Username does not exist")
 
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
-            return redirect('profiles')
+            return redirect("profiles")
         else:
-            messages.error(request, 'Username or password is incorrect')
+            messages.error(request, "Username or password is incorrect")
 
     return render(request, "users/login_register.html")
 
 
 def logoutUser(request):
     logout(request)
-    messages.error(request, 'User logout!')
-    return redirect('login')
+    messages.success(request, "User logout!")
+    return redirect("login")
+
+
+def registerUser(request):
+    page = "register"
+    form = UserCreationForm
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            messages.success(request, "User registered!")
+
+    context = {"page": page, "form": form}
+    return render(request, "users/login_register.html", context)
 
 
 def profiles(request):
