273b91a4e00e522852e4080b56953dba7eb4b496
grzegorz-oledzki <you@example.com>
2022-03-02 20:08:41 +0100

    add pagination to outside html, add pagination to users

--- users/utils.py
+++ users/utils.py
@@ -1,8 +1,35 @@
 from .models import Profile, Skill
 from django.db.models import Q
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 
-def searchProfiles(request):
+def pagination_project(request, players, results_on_page):
+    page = request.GET.get("page")
+    paginator = Paginator(players, results_on_page)
+
+    try:
+        players = paginator.page(page)
+    except PageNotAnInteger:
+        page = 1
+        players = paginator.page(page)
+    except EmptyPage:
+        page = paginator.num_pages
+        players = paginator.page(page)
+
+    left_index = int(page) - 1
+    if left_index < 1:
+        left_index = 1
+
+    right_index = int(page) + 2
+    if right_index > paginator.num_pages:
+        right_index = paginator.num_pages + 1
+
+    custom_range = range(left_index, right_index)
+
+    return custom_range, players
+
+
+def search_profiles(request):
     search_query = ""
 
     if request.GET.get("search_query"):
