0c5dbd4cfe7b10da5e09722033da915dc40ce928
grzegorz-oledzki <you@example.com>
2022-03-03 20:55:37 +0100

    add as email can be null

--- users/models.py
+++ users/models.py
@@ -8,7 +8,8 @@ class Profile(models.Model):
     name = models.CharField(max_length=100, blank=True, null=True)
     username = models.CharField(max_length=100, blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True)
-    email = models.EmailField(max_length=100, blank=True)
+    email = models.EmailField(max_length=100, blank=True, null=True
+                              )
     short_intro = models.CharField(max_length=250, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
     profile_image = models.ImageField(
