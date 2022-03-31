d03d3ae4af36a761af9aafaa3312fab12ead9724
grzegorz-oledzki <you@example.com>
2022-03-31 21:01:44 +0200

    balck formatting

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path("", views.get_routes),
     path("players/", views.get_players),
     path("player/<str:pk>", views.get_player),
-
-    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path("users/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("users/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
