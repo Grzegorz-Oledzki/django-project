4ff2b4e8dfcd7f1daa0f7b45a9adad04d61dfc59
grzegorz-oledzki <you@example.com>
2022-03-31 21:01:04 +0200

    add postman and jwt token configuration

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("", views.get_routes),
     path("players/", views.get_players),
     path("player/<str:pk>", views.get_player),
+
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
