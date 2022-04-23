18a838963801fa788d25a914d163cc7178152dae
grzegorz-oledzki <you@example.com>
2022-04-23 13:39:06 +0200

    make it better

--- django_project/settings.py
+++ django_project/settings.py
@@ -202,6 +202,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+AWS_S3_FILE_OVERWRITE = False
 AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
