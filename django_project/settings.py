07ee67b3cb120d916a04cd78aecb80109e453abf
grzegorz-oledzki <you@example.com>
2022-04-23 17:51:33 +0200

    database change2

--- django_project/settings.py
+++ django_project/settings.py
@@ -121,24 +121,24 @@ WSGI_APPLICATION = "django_project.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "grzes_db",
-#         "USER": 'grzes',
-#         "PASSWORD": config("PASSWORD"),
-#         "HOST": "database-1.c8xwyysae5xe.eu-west-1.rds.amazonaws.com",
-#         "PORT": "5432",
-#     }
-# }
-
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "grzes_db",
+        "USER": 'grzes',
+        "PASSWORD": config("PASSWORD"),
+        "HOST": "database-1.c8xwyysae5xe.eu-west-1.rds.amazonaws.com",
+        "PORT": "5432",
     }
 }
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
